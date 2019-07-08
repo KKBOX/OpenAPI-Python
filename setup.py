eb17604561daf5198ccaecbdca244184f6198a67
Willy Liu <willyliu@kkbox.com>
2019-07-08 10:15:48 +0800

    Bumps version to 1.0.7.

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ from distutils.core import setup
 setup(name='kkbox_developer_sdk',
       description='KKBOX Open API Developer SDK for Python',
       author='KKBOX',
-      author_email='sharonyang@kkbox.com',
-      version='1.0.6',
+      author_email='willyliu@kkbox.com',
+      version='1.0.7',
       url='https://github.com/KKBOX/OpenAPI-Python',
-      download_url='https://github.com/KKBOX/OpenAPI-Python/tarball/v1.0.6',
+      download_url='https://github.com/KKBOX/OpenAPI-Python/tarball/v1.0.7',
       packages=['kkbox_developer_sdk'],
       package_dir={'kkbox_developer_sdk': 'kkbox_developer_sdk'},
       keywords=['KKBOX', 'Open', 'API', 'OpenAPI'])
